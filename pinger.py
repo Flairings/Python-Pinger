e3bbd4ef1664e4f58aebc2cf7e548ea1a088dfcf
$~Swag <58793605+Flairings@users.noreply.github.com>
2021-01-25 19:09:55 +0000

    added offline and online colors

--- pinger.py
+++ pinger.py
@@ -50,6 +50,8 @@ class GracefulInterruptHandler(object):
 with open("config.json", "r") as settings:
     config = json.load(settings)
     color = config.get("color")
+    online_color = config.get("online-color")
+    offline_color = config.get("offline-color")
     online = config.get("online")
     offline = config.get("offline")
 
@@ -81,8 +83,67 @@ elif color == "BLACK":
     color = Fore.BLACK
 else:
     color = Fore.LIGHTWHITE_EX
-    print("INVALID COLOR.")
+    print("color is invalid.")
 
+if online_color == "RED":
+    online_color = Fore.RED
+elif online_color == "LIGHT_RED":
+    online_color = Fore.LIGHTRED_EX
+elif online_color == "BLUE":
+    online_color = Fore.BLUE
+elif online_color == "LIGHT_BLUE":
+    online_color = Fore.LIGHTBLUE_EX
+elif online_color == "MAGENTA":
+    online_color = Fore.MAGENTA
+elif online_color == "LIGHT_MAGENTA":
+    online_color = Fore.LIGHTMAGENTA_EX
+elif online_color == "YELLOW":
+    online_color = Fore.YELLOW
+elif online_color == "LIGHT_YELLOW":
+    online_color = Fore.LIGHTYELLOW_EX
+elif online_color == "GREEN":
+    online_color = Fore.GREEN
+elif online_color == "LIGHT_GREEN":
+    online_color = Fore.LIGHTGREEN_EX
+elif online_color == "CYAN":
+    online_color = Fore.CYAN
+elif online_color == "LIGHT_CYAN":
+    online_color = Fore.LIGHTCYAN_EX
+elif online_color == "BLACK":
+    online_color = Fore.BLACK
+else:
+    online_color = Fore.LIGHTWHITE_EX
+    print("online_color is invalid.")
+
+if offline_color == "RED":
+    offline_color = Fore.RED
+elif offline_color == "LIGHT_RED":
+    offline_color = Fore.LIGHTRED_EX
+elif offline_color == "BLUE":
+    offline_color = Fore.BLUE
+elif offline_color == "LIGHT_BLUE":
+    offline_color = Fore.LIGHTBLUE_EX
+elif offline_color == "MAGENTA":
+    offline_color = Fore.MAGENTA
+elif offline_color == "LIGHT_MAGENTA":
+    offline_color = Fore.LIGHTMAGENTA_EX
+elif offline_color == "YELLOW":
+    offline_color = Fore.YELLOW
+elif offline_color == "LIGHT_YELLOW":
+    offline_color = Fore.LIGHTYELLOW_EX
+elif offline_color == "GREEN":
+    offline_color = Fore.GREEN
+elif offline_color == "LIGHT_GREEN":
+    offline_color = Fore.LIGHTGREEN_EX
+elif offline_color == "CYAN":
+    offline_color = Fore.CYAN
+elif offline_color == "LIGHT_CYAN":
+    offline_color = Fore.LIGHTCYAN_EX
+elif offline_color == "BLACK":
+    offline_color = Fore.BLACK
+else:
+    offline_color = Fore.LIGHTWHITE_EX
+    print("offline_color is invalid.")
 
 def start():
     clear()
@@ -96,13 +157,13 @@ def start():
             try:
                 host = ping(address, count=1, interval=0.1)
                 if host.is_alive:
-                    print(f"{color}{online}"
+                    print(f"{online_color}{online}"
                           .replace("{host.address}", host.address)
                           .replace("{host.min_rtt}", (str(host.min_rtt)))
                           .replace("{host.avg_rtt}", (str(host.min_rtt)))
                           .replace("{host.max_rtt}", (str(host.max_rtt))))
                 else:
-                    print(f"{color}{offline}"
+                    print(f"{offline_color}{offline}"
                           .replace("{host.address}", host.address)
                           .replace("{host.min_rtt}", (str(host.min_rtt)))
                           .replace("{host.avg_rtt}", (str(host.min_rtt)))
